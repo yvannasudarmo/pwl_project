91793c9af609e144fd7d528725640bde125a896e
yvannasudarmo <yvannasudarmo@itbss.ac.id>
2026-05-28 02:01:51 +0700

    commit

--- resources/views/matakuliah/edit.blade.php
+++ resources/views/matakuliah/edit.blade.php
@@ -4,27 +4,27 @@
         <tr>
             <td>Jurusan</td>
             <td>:</td>
-            <td><input type="text" name="Jurusan_Id"></td>
+            <td><input type="text" name="Jurusan_Id" value="{{$matakuliah->Jurusan_Id}}"></td>
         </tr>
         <tr>
             <td>Kode Mata Kuliah</td>
             <td>:</td>
-            <td><input type="text" name="Kode_MK"></td>
+            <td><input type="text" name="Kode_MK" value="{{$matakuliah->Kode_MK}}"></td>
         </tr>
         <tr>
             <td>Nama Mata Kuliah</td>
             <td>:</td>
-            <td><input type="text" name="Nama_MK"></td>
+            <td><input type="text" name="Nama_MK" value="{{$matakuliah->Nama_MK}}"></td>
         </tr>
         <tr>
             <td>SKS</td>
             <td>:</td>
-            <td><input type="text" name="SKS"></td>
+            <td><input type="text" name="SKS" value="{{$matakuliah->SKS}}"></td>
         </tr>
         <tr>
             <td>Kode Dosen</td>
             <td>:</td>
-            <td><input type="text" name="Kode_Dosen"></td>
+            <td><input type="text" name="Kode_Dosen" value="{{$matakuliah->Kode_Dosen}}"></td>
         </tr>
     </table>
 
