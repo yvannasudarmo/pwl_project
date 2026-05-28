99533b75697f3060fbd5557a388e09ad466c3845
yvannasudarmo <yvannasudarmo@itbss.ac.id>
2026-05-28 17:42:36 +0700

    commit

--- resources/views/matakuliah/edit.blade.php
+++ resources/views/matakuliah/edit.blade.php
@@ -29,7 +29,7 @@
             <td>:</td>
             <td><input type="text" name="Dosen_Id" value="{{$matakuliah->Dosen_Id}}"></td>
         </tr>
+            </table>
 <button type="Submit">Add</button>
 <button type="reset"> Clear </button>
-    </table>
 </form>
