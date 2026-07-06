cdeba7964da1c5a74d22df62b56a9d80d5c35955
yvannasudarmo <yvannasudarmo@itbss.ac.id>
2026-07-06 14:08:12 +0700

    commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -129,6 +129,6 @@ class AuthController extends Controller
     $request->session()->invalidate();
     $request->session()->regenerateToken();
 
-    return redirect()->route('dashboard');
+    return redirect()->route('landing');
 }
 }
