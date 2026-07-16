ba891038b4d8ebfb31c276801cee3494852d8f21
yvannasudarmo <yvannasudarmo@itbss.ac.id>
2026-07-16 15:46:38 +0700

    commit

--- app/Http/Controllers/KRSController.php
+++ app/Http/Controllers/KRSController.php
@@ -43,6 +43,7 @@ class KRSController extends Controller
      */
 public function store(Request $request)
     {
+        dd($request->all()); // <-- Tambahkan ini sementara, lalu SIMPAN file code Anda
         $data = [
             // Memetakan ke kolom database 'NIM' dari berbagai kemungkinan nama input HTML form Anda
             'NIM'          => $request->NIM ?? $request->nim ?? $request->kode_mahasiswa ?? $request->mahasiswa_id,
