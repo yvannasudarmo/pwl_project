46ef4e61f1fe5a3b89995b314af585fb111d5c79
yvannasudarmo <yvannasudarmo@itbss.ac.id>
2026-07-16 15:17:04 +0700

    commit

--- app/Http/Controllers/KelasController.php
+++ app/Http/Controllers/KelasController.php
@@ -95,8 +95,9 @@ class KelasController extends Controller
             'tahun_ajaran' => $request->tahun_ajaran,
             'semester'     => $request->semester,
             'jumlah_max'   => $request->jumlah_max,
-            'jumlah_mahasiswa' => $request->jumlah_mahasiswa,
-        ];
+
+'jumlah_mahasiswa' => 0,
+            ];
 
         Kelas::findOrFail($id)->update($data);
 
